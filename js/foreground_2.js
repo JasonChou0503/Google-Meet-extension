e757b0e477edd057446a24231847a683dfe77687
JasonChou0503 <jason940503@gmail.com>
2022-06-06 01:56:52 +0800

    init

---
+++ js/foreground_2.js
@@ -0,0 +1,14 @@
+console.log(`>>> foreground_2 online`);
+
+setTimeout(function(){ // 等待loading
+    document.querySelector("#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.ZB88ed > div > div > div.U26fgb.JRY2Pb.mUbCce.kpROve.yBiuPb.y1zVCf.HNeRed.M9Bg4d").click() // 關聲音
+    
+    document.querySelector("#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c.XDitY > div > div.EhAUAc > div.GOH7Zb > div > div.U26fgb.JRY2Pb.mUbCce.kpROve.yBiuPb.y1zVCf.M9Bg4d.HNeRed").click() // 關鏡頭
+    document.querySelector("#yDmH0d > c-wiz > div > div > div:nth-child(10) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div:nth-child(1) > button > div.VfPpkd-RLmnJb").click() // 進入會議
+    setTimeout(function(){
+        document.querySelector("#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div.UnvNgf.Sdwpn.P9KVBf > div.Tmb7Fd > div > div.NHaLPe > span > button").click() // 退出
+        setTimeout(function(){
+            document.querySelector("#ow3 > div > div > div.kJU3pb.r14hdb > div:nth-child(2) > div > button > div.VfPpkd-RLmnJb").click()
+        },2000)
+    }, 3000000) // 3000000 ms
+}, 2000)
